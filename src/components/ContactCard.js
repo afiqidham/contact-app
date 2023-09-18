2641867c2c5acbb8d309a5e35878fd20a59a4c31
afiqidham <afiq.zaki25@gmail.com>
2023-09-18 19:47:43 +0800

    new

---
+++ src/components/ContactCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import user from "../images/user.png";
+
+const ContactCard = (props) => {
+    const {id, name, email} = props.contact;
+    return(
+        <div className="item">
+            <img className="ui avatar image" src={user} alt="user" style={{ marginTop:"10px" }}/>
+                <div className="content">
+                    <div className="header">
+                        {name}
+                    </div>
+                    <div>
+                        {email}
+                    </div>
+                    <i 
+                    className="trash alternate outline icon" 
+                    onClick={()=> props.clickHandler(id)}
+                    style={{ color:"red", marginTop:"7px" }}
+                    ></i>
+                </div>
+            </div>
+    );
+};
+
+export default ContactCard;
